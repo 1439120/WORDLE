e1d0b03c4d552bc4dcbeb69ca8733afe552202ba
1439120 <nkosana.mabuza2@gmail.com>
2022-05-09 17:45:54 +0100

    changing colours for input

--- client/src/component/Board.js
+++ client/src/component/Board.js
@@ -3,27 +3,34 @@ import React, {useState, useEffect} from 'react';
 
 
 function Board(){
-
+    // 
+    let word_ = "QUEEN"
     let rows = initializeBoard(5,6)
     const [index, setIndex] = useState(0)
-    const [gameOver, setGameOver] = useState(false)
+    //const [gameOver, setGameOver] = useState(false)
 
     function handler(e){
         if(index < 5 * 6 && (e.code >= "KeyA" && e.code <= "KeyZ")){
             setIndex(index + 1)
             document.getElementById(index.toString()).innerHTML = e.code[3]
 
-            // now i need to make sure a complete row is 
-            // completed and enter is pressed before going to the next row
+            // Check if the alphabet is correct
+            if(word_[index % 5] === e.code[3]){
+                document.getElementById(index.toString()).id = "Correct"
+            }else if(word_.indexOf(e.code[3]) >= 0){
+                document.getElementById(index.toString()).id = "Present"
+            }else if(word_.indexOf(e.code[3]) === -1){
+                document.getElementById(index.toString()).id = "Absent"
+            }
         }
-        
     }
 
     function gameStatus(e){
         console.log(e)
         if(e.key === "Enter" ){
+            // now i need to make sure a complete row is 
+            // completed and enter is pressed before going to the next row
             window.addEventListener("keyup", handler)
-            
         }
     }
 
